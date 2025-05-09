38ffd9da96e22cbd931e973ebaf31df6f4bf1db1
Willian Ribeiro <willian.ribeiro@icarotech.com>
2025-05-09 14:34:38 -0300

    Criando Cenarios de Teste de API

---
+++ cypress/integration/api/001_fluxo_criacao_de_produtos_.spec.js
@@ -0,0 +1,79 @@
+// importa as classes e instancia das classes
+import { EndpointsApi } from "../../support/helpers/endpoints-envs";
+const endpoint = new EndpointsApi
+
+import { CriarProduto } from "../../support/steps/api/001_criar_novo_produto_POST_";
+const criar = new CriarProduto
+
+import { ListarProduto } from "../../support/steps/api/002_listar_todos_produtos_GET_";
+const listar = new ListarProduto
+
+//Bloco de teste que agrupa os casos
+describe('Validar criação de produto e listagem', () => {
+    
+    // primeiro caso de teste
+    it('001 - Criar novo produto', () => {
+
+        // criar um objeto de "Dados" 
+        var dados = {
+            endpoint: endpoint.endpointProduto(),
+            name: 'Smartphone',
+            brand: 'Samsung',
+            price: 800
+        }
+
+        // criar um objeto de "Assert"
+        var assert = {
+            status: 201,
+            name: 'Smartphone',
+            brand: 'Samsung',
+            price: 800
+        }
+
+        // chama a função para criar nosso produto
+        criar.criarProduto(dados, assert)
+    })
+
+
+    it('002 - Criar novo produto sem inserir o nome', () => {
+
+        // criar um objeto de "Dados" 
+        var dados = {
+            endpoint: endpoint.endpointProduto(),
+            name: '',
+            brand: 'Samsung',
+            price: 400
+        }
+
+        // criar um objeto de "Assert"
+        var assert = {
+            status: 400,
+            message: 'Name, brand, and price are required.'
+        }
+
+        // chama a função para criar nosso produto
+        criar.criarProduto(dados, assert)
+    })
+
+
+    it('003 - Listar produto criado', () => {
+
+        // criar um objeto de "Dados" 
+        var dados = {
+            endpoint: endpoint.endpointProduto(),
+        }
+
+        // criar um objeto de "Assert"
+        var assert = {
+            status: 200,
+            id: 21,
+            name: 'Smartphone',
+            brand: 'Samsung',
+            price: 800
+        }
+
+        // chama a função para listar produto
+        listar.listarProduto(dados, assert)
+    })
+
+})
